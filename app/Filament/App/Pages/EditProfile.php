7ee12470c05ac43f3a5236a53002efd844e46193
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-01 02:04:14 +0000

    Configure new App Panel

--- app/Filament/App/Pages/EditProfile.php
+++ app/Filament/App/Pages/EditProfile.php
@@ -2,23 +2,60 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\User;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Page;
+use Illuminate\Support\Facades\Auth;
 
 class EditProfile extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
-
     protected static string $view = 'filament.pages.edit-profile';
+    protected static ?string $navigationIcon = 'heroicon-o-chart-bar';
 
-    protected static ?string $navigationLabel = 'Profile';
+    public User $user;
 
-    public static function shouldRegisterNavigation(): bool
+    public function mount()
     {
-        return false;
+        $this->user = Auth::user();
+        $this->form->fill([
+            'name'  => $this->user->name,
+            'email' => $this->user->email,
+        ]);
     }
 
-    public static function getNavigationSort(): ?int
+    protected function getFormSchema(): array
     {
-        return 1;
+        return [
+            TextInput::make('name')
+                ->label('Name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('email')
+                ->label('Email Address')
+                ->required()
+                ->maxLength(255),
+        ];
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        $state = $this->form->getState();
+
+        $this->user->forceFill([
+            'name'  => $state['name'],
+            'email' => $state['email'],
+        ])->save();
+
+        Filament::notify('success', 'Your profile has been updated.');
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return [
+            url()->current() => 'Edit Profile',
+        ];
     }
 }
--- app/Filament/App/Pages/EditTeam.php
+++ app/Filament/App/Pages/EditTeam.php
@@ -2,24 +2,63 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Team;
+use App\Models\User;
 use Filament\Facades\Filament;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Tenancy\EditTenantProfile;
 
 class EditTeam extends EditTenantProfile
 {
     protected static string $view = 'filament.pages.edit-team';
 
-    protected static ?int $navigationSort = 2;
+    public $name = '';
 
     public static function getLabel(): string
     {
-        return 'Team Settings';
+        return 'Edit Team';
     }
 
-    protected function getViewData(): array
+    public function mount(): void
+    {
+        abort_unless($this->user()->canCreateTeams(), 403);
+    }
+
+    protected function getFormSchema(): array
     {
         return [
-            'team' => Filament::getTenant(),
+            TextInput::make('name')
+                ->label('Team Name')
+                ->required()
+                ->maxLength(255),
         ];
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        $team = Team::forceCreate([
+            'user_id'       => Filament::auth()->id(),
+            'name'          => $this->name,
+            'personal_team' => false,
+        ]);
+
+        $this->user()->teams()->attach($team, ['role' => 'admin']);
+        $this->user()->switchTeam($team);
+
+        return redirect()->route('filament.pages.edit-team', ['team' => $team]);
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return [
+            url()->current() => 'Create Team',
+        ];
+    }
+
+    private function user(): User
+    {
+        return Filament::auth()->user();
     }
 }
