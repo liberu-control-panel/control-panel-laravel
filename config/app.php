0b3e91d5d58e65b929b13871cc554b40e277bd16
Curtis Delicata <curtis.delicata@liberu.co.uk>
2025-06-01 13:42:47 +0000

    Update dependencies

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\TeamServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
+    App\Providers\SiteSettingsServiceProvider::class,
 
         /*
          * Docker-specific Service Providers...
